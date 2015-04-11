d2b3765e6bac8989a19e86adc2c9dc5e7ea14130
Jonathan Martinez <jamm@jmartinezm.org>
2015-04-10 23:20:41 -0430

    Hecho el merge, agreagado el que al borrar un player se borren sus characters

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,5 @@
 class Player < ActiveRecord::Base
-<<<<<<< HEAD
-  has_many :characters
-=======
-  has_many :character, dependent: :destroy
->>>>>>> 77196f371fca7fc59a3870626466bb61785ca909
-
+  has_many :characters, dependent: :destroy
   validates :name, :last_name, :email, presence: true
   validates :name, :last_name, format: { :with => /\A[a-z A-Z]+\z/}
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
